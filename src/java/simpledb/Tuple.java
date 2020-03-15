77d591689eefaf667131871d0e8d4d460cfafcfb
Jimmy Young <yangjufo@gmail.com>
2020-03-15 13:30:16 -0700

    add Tuple

--- src/java/simpledb/Tuple.java
+++ src/java/simpledb/Tuple.java
@@ -1,8 +1,8 @@
 package simpledb;
 
 import java.io.Serializable;
-import java.util.Arrays;
-import java.util.Iterator;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * Tuple maintains information about the contents of a tuple. Tuples have a
@@ -13,95 +13,87 @@ public class Tuple implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    private TupleDesc td;
+    private RecordId rid;
+    private final List<Field> fields;
+
     /**
      * Create a new tuple with the specified schema (type).
      *
-     * @param td
-     *            the schema of this tuple. It must be a valid TupleDesc
-     *            instance with at least one field.
+     * @param td the schema of this tuple. It must be a valid TupleDesc
+     *           instance with at least one field.
      */
-    public Tuple(TupleDesc td) {
-        // some code goes here
+    public Tuple(final TupleDesc td) {
+        this.td = td;
+        fields = new LinkedList<>(Collections.nCopies(td.numFields(), null));
+        rid = null;
     }
 
     /**
      * @return The TupleDesc representing the schema of this tuple.
      */
     public TupleDesc getTupleDesc() {
-        // some code goes here
-        return null;
+        return td;
     }
 
     /**
      * @return The RecordId representing the location of this tuple on disk. May
-     *         be null.
+     * be null.
      */
     public RecordId getRecordId() {
-        // some code goes here
-        return null;
+        return rid;
     }
 
     /**
      * Set the RecordId information for this tuple.
      *
-     * @param rid
-     *            the new RecordId for this tuple.
+     * @param rid the new RecordId for this tuple.
      */
-    public void setRecordId(RecordId rid) {
-        // some code goes here
+    public void setRecordId(final RecordId rid) {
+        this.rid = rid;
     }
 
     /**
      * Change the value of the ith field of this tuple.
      *
-     * @param i
-     *            index of the field to change. It must be a valid index.
-     * @param f
-     *            new value for the field.
+     * @param i index of the field to change. It must be a valid index.
+     * @param f new value for the field.
      */
-    public void setField(int i, Field f) {
-        // some code goes here
+    public void setField(final int i, final Field f) {
+        fields.set(i, f);
     }
 
     /**
+     * @param i field index to return. Must be a valid index.
      * @return the value of the ith field, or null if it has not been set.
-     *
-     * @param i
-     *            field index to return. Must be a valid index.
      */
-    public Field getField(int i) {
-        // some code goes here
-        return null;
+    public Field getField(final int i) {
+        return fields.get(i);
     }
 
     /**
      * Returns the contents of this Tuple as a string. Note that to pass the
      * system tests, the format needs to be as follows:
-     *
+     * <p>
      * column1\tcolumn2\tcolumn3\t...\tcolumnN
-     *
+     * <p>
      * where \t is any whitespace (except a newline)
      */
     public String toString() {
-        // some code goes here
-        throw new UnsupportedOperationException("Implement this");
+        return fields.stream().map(Field::toString).collect(Collectors.joining("\t"));
     }
 
     /**
-     * @return
-     *        An iterator which iterates over all the fields of this tuple
-     * */
-    public Iterator<Field> fields()
-    {
-        // some code goes here
-        return null;
+     * @return An iterator which iterates over all the fields of this tuple
+     */
+    public Iterator<Field> fields() {
+        return fields.iterator();
     }
 
     /**
      * reset the TupleDesc of this tuple (only affecting the TupleDesc)
-     * */
-    public void resetTupleDesc(TupleDesc td)
-    {
-        // some code goes here
+     */
+    public void resetTupleDesc(final TupleDesc td) {
+        this.td = td;
     }
 }
